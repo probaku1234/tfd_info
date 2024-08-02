eb3869c9914535ccf30c7a7f70bcea2a8f47b036
Yun Seob Lee <fhzotxldj@gmail.com>
2024-08-02 18:17:24 +0900

    add font

--- front/src/components/layout.tsx
+++ front/src/components/layout.tsx
@@ -1,9 +1,10 @@
-import React, { ReactNode } from 'react';
-import { Flex } from '@chakra-ui/react';
-import Header from './header'
-import Footer from './footer';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import React, { ReactNode } from "react";
+import { Flex } from "@chakra-ui/react";
+import Header from "./header";
+import Footer from "./footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./layout.css";
 
 type LayoutProps = {
   children: ReactNode;
@@ -11,9 +12,20 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Flex direction="column" minH="100vh" backgroundImage={'url("/images/background.png")'}>
+    <Flex
+      direction="column"
+      minH="100vh"
+      backgroundImage={'url("/images/background.png")'}
+    >
       <Header />
-      <Flex as="main" direction="column" align="center" justify="center" flex="1" p={4}>
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        flex="1"
+        p={4}
+      >
         {children}
         <Analytics />
         <SpeedInsights />
--- front/src/theme/index.ts
+++ front/src/theme/index.ts
@@ -2,5 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 // Or export `extendBaseTheme` if you only want the default Chakra theme tokens to extend (no default component themes)
 export const customTheme = extendTheme({
-  
+  fonts: {
+    heading: "'Do Hyeon', sans-serif",
+    body: "'Do Hyeon', sans-serif",
+  }
 });
