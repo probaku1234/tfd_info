1fb6ca579f9720fd63d3251c571e98529952aea2
Yun Seob Lee <fhzotxldj@gmail.com>
2024-08-04 15:12:43 +0900

    add og and twitter seo tags

--- front/src/components/seo.tsx
+++ front/src/components/seo.tsx
@@ -35,8 +35,26 @@ export const SEO: React.FC<SEOProps> = ({
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-      <meta name='url' content={seo.url} />
-      <meta name='keywords' content={seo.keywords} />
+      <meta name="url" content={seo.url} />
+      <meta name="keywords" content={seo.keywords} />
+
+      {/*  */}
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={seo.title} />
+      <meta
+        property="og:description"
+        content={seo.description}
+      />
+      <meta property="og:image" content={"/images/main2.png"} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content={"/images/main2.png"} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+
       {children}
     </>
   );
