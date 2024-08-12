7070c460217b94ca7a4c852cde19799bd9096db8
Yun Seob Lee <fhzotxldj@gmail.com>
2024-08-12 12:42:47 +0900

    update 404 page

--- front/src/pages/404.tsx
+++ front/src/pages/404.tsx
@@ -8,38 +8,66 @@ import {
   Text,
   Button,
 } from "@chakra-ui/react";
+import Layout from "../components/layout";
+import LocaleContext from "../context/locale_context";
 
 const NotFoundPage: React.FC<PageProps> = () => {
+  const localeContext = React.useContext(LocaleContext);
+  const { locale } = localeContext!;
+
+  const getTranslation = (locale: string) => {
+    return translation[locale] || translation.en;
+  };
+
+  const { page_not_found, message } = getTranslation(locale);
+
   return (
-    <Box p={24} fontFamily="-apple-system, Roboto, sans-serif, serif">
-      <Heading as="h1" mt={0} mb={16} maxW="container.sm">
-        Page not found
-      </Heading>
-      <Text mb={12}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in{" "}
-            <Code colorScheme="yellow" p={1} fontSize="lg" borderRadius="base">
-              src/pages/
-            </Code>
-            .
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Button>
-          <ChakraLink as={Link} to="/">
-            Go home
-          </ChakraLink>
-        </Button>
-      </Text>
-    </Box>
+    <Layout>
+      <Box textAlign={"center"} position={"relative"} textColor={"white"}>
+        <Heading as="h1" fontSize={"20em"}>
+          404
+        </Heading>
+        <Heading as="h1" mt={0} mb={16} maxW="container.sm" fontSize={"4em"}>
+          {page_not_found}
+        </Heading>
+        <Text mb={12} fontSize={"2xl"}>
+          {message}
+          <br />
+          {process.env.NODE_ENV === "development" ? (
+            <>
+              <br />
+              Try creating a page in{" "}
+              <Code
+                colorScheme="yellow"
+                p={1}
+                fontSize="lg"
+                borderRadius="base"
+              >
+                src/pages/
+              </Code>
+              .
+              <br />
+            </>
+          ) : null}
+        </Text>
+      </Box>
+    </Layout>
   );
 };
 
 export default NotFoundPage;
 
 export const Head: HeadFC = () => <title>Not found</title>;
+
+const translation: {
+  [key: string]: { page_not_found: string; message: string };
+} = {
+  ko: {
+    page_not_found: "페이지를 찾을 수 없습니다.",
+    message: "죄송합니다 😔, 원하시는 페이지를 찾을 수 없습니다.",
+  },
+  en: {
+    page_not_found: "Page not found",
+    message: "Sorry 😔, we couldn’t find what you were looking for.",
+  },
+};
